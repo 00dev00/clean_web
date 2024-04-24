c7a18d96db6e5ce2f6ecd547bfaf4f7c53bbf5fd
denisk <progdegree@gmail.com>
2024-04-24 15:43:05 +0300

    fix static files serving, some minor changes of supplementary files

--- API/Program.cs
+++ API/Program.cs
@@ -5,29 +5,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(
     options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
+app.UseStaticFiles();
+app.UseAuthorization();
 app.MapControllers();
+app.MapHealthChecks("/healthz");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## WIP: Небольшой проект ASP.NET Core 8 (Clean architecture, немного паттернов с DDD, Angular)
+### ***WIP***: Небольшой проект ASP.NET Core 8 (Clean architecture, немного паттернов с DDD, Angular)
